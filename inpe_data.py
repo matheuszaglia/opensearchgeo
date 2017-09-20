43fd1b9908ad8e81820ed833d80894ed8eb12615
Matheus C. Zaglia <mzaglia@dpi.inpe.br>
2017-09-20 14:05:41 -0300

    Adding two-step search, geo:uid and more

--- inpe_data.py
+++ inpe_data.py
@@ -1,37 +1,68 @@
-from collections import OrderedDict
 from db_connection import database
 
 
-def get_bbox(bbox, search_terms, time_start, time_end, start, count):
-    minX, minY,maxX, maxY = bbox.split(',')
-    sql = """
-    SELECT  *, DATE_FORMAT(`Date`,'%%Y-%%m-%%dT%%H:%%i:%%s-03:00') as `Date`,
-    DATE_FORMAT(`IngestDate`,'%%Y-%%m-%%dT%%H:%%i:%%s-03:00') as `IngestDate` 
-    FROM Scene WHERE"""
+def get_bbox(bbox, uid, dataset_id, time_start, time_end, start, count):
 
-    where = """ {}<= `TL_Latitude` and {} <=`TL_Longitude` and {} >=`TR_Latitude` and {} <= `TR_Longitude`
-                and {} >=`BR_Latitude` and {} >= `BR_Longitude` and {} <= `BL_Latitude` and {} >=`BL_Longitude`"""\
-                .format(minY, minX, maxY, minX, maxY, maxX, minY, maxX)
+    sql = "SELECT *, DATE_FORMAT(`Date`,'%%Y-%%m-%%dT%%H:%%i:%%s') as `Date`, " \
+          "DATE_FORMAT(`IngestDate`,'%%Y-%%m-%%dT%%H:%%i:%%s') as `IngestDate` " \
+          "FROM Scene WHERE "
+
+    where = []
+
+    if uid is not None:
+        where.append("`SceneId` = '{}'".format(uid))
+
+    if bbox is not None:
+        try:
+            min_x, min_y, max_x, max_y = bbox.split(',')
+            for x in bbox.split(','):
+                float(x)
+
+            where.append("{}<= `TL_Latitude` and {} <=`TL_Longitude` and {} >=`TR_Latitude` and" \
+                   " {} <= `TR_Longitude` and {} >=`BR_Latitude` and {} >= `BR_Longitude` and" \
+                   " {} <= `BL_Latitude` and {} >=`BL_Longitude`".format(min_y, min_x, max_y, min_x,
+                                                                         max_y, max_x, min_y, max_x))
+        except:
+            raise (InvalidBoundingBoxError())
+
+    if dataset_id is not None:
+        where.append("CONCAT(`Satellite`, `Sensor`) = '{}'".format(dataset_id))
+
     if time_start is not None:
-        where += """ and `Date` >= '{}'""".format(time_start)
-    if time_end is not None:
-        where+= """ and `Date`<= '{}'""".format(time_end)
-    else:
-        where+= """ and `Date` <= curdate()"""
+        where.append("`Date` >= '{}'".format(time_start))
 
-    where+=""" ORDER BY `Date` DESC"""
-    result = database('login', 'password', 'host', 'database').do_query(sql+where)
-    sql_len = """SELECT COUNT(`SceneId`) FROM Scene WHERE {} """.format(where)
-    result_len = database('login', 'password', 'host', 'database').do_query(sql_len)
-    return prepare_dict(result, start, count), len(result)
+    if time_end is not None:
+        where.append("`Date`<= '{}'".format(time_end))
+
+    else:
+        where.append("`Date` <= curdate()")
+
+    where = " and ".join(where)
+
+    sql += where
+
+    sql += " ORDER BY `Date` DESC"
+
+    sql += " LIMIT {},{}".format(start, count)
+
+    result = database('login', 'password', 'host', 'database').do_query(sql)
+
+    sql = "SELECT COUNT(`SceneId`) " \
+          "FROM Scene " \
+          "WHERE {} ".format(where)
+
+    result_len = database('login', 'password', 'host', 'database').do_query(sql)
+    if result_len[0][0] < count:
+        count = result_len[0][0]
+
+    return prepare_dict(result, start, count), result_len[0][0]
 
 
 def get_scene(sceneid):
-    sql = """
-    SELECT  *, DATE_FORMAT(`Date`,'%%Y-%%m-%%dT%%H:%%i:%%s-03:00') as `Date`,
-    DATE_FORMAT(`IngestDate`,'%%Y-%%m-%%dT%%H:%%i:%%s-03:00') as `IngestDate`
-    FROM Scene
-    WHERE `SceneId`='{}'""".format(sceneid)
+    sql = "SELECT  *, DATE_FORMAT(`Date`,'%%Y-%%m-%%dT%%H:%%i:%%s') as `Date`, " \
+          "DATE_FORMAT(`IngestDate`,'%%Y-%%m-%%dT%%H:%%i:%%s') as `IngestDate` " \
+          "FROM Scene" \
+          "WHERE `SceneId`='{}'".format(sceneid)
 
     result = database('login', 'password', 'host', 'database').do_query(sql)
     data = dict(result[0].items())
@@ -39,20 +70,43 @@ def get_scene(sceneid):
     return data
 
 
+def get_updated():
+    sql = "SELECT DATE_FORMAT(`update_time`,'%%Y-%%m-%%dT%%H:%%i:%%s') as `Date` " \
+          "FROM information_schema.tables WHERE table_name = 'Scene';"
+
+    result = database('login', 'password', 'host', 'database').do_query(sql)
+
+    return result[0][0]
+
+
 def prepare_dict(data, start, count):
     res = []
-    for i in range(start, start+count):
+    for i in range(start, start + count):
         res.append(dict(data[i].items()))
         prefix = None
         url_prefix = 'http://www.dgi.inpe.br/'
         res[-1]['browseURL'] = "opensearch/browseimage/{}".format(data[i]['SceneId'])
-        res[-1]['cartURL'] = url_prefix + 'CDSR/cart-cwic.php?SCENEID='+ data[i]['SceneId']
+        res[-1]['cartURL'] = url_prefix + 'CDSR/cart-cwic.php?SCENEID=' + data[i]['SceneId']
     return res
 
 
-def get_collections():
-    return ('INPE_CBERS4_AWFI', 'INPE_CBERS4_MUX', 'INPE_CBERS4_PAN5M', 'INPE_CBERS4_PAN10M',
-            'INPE_CBERS4_IRS', 'INPE_ALL')
+def get_datasets(query, uid, start_index, count):
+    where = ""
+
+    if uid is not None:
+        where = " WHERE CONCAT(`Satellite`, `Sensor`) = '{}'".format(uid)
+    elif query is not None:
+        where = " WHERE CONCAT(`Satellite`, `Sensor`) LIKE '%%{}%%'".format(query)
+
+    sql = "SELECT `Satellite`, `Sensor`, " \
+          "MAX(DATE_FORMAT(`Date`,'%%Y-%%m-%%dT%%H:%%i:%%s')) as `Date` " \
+          "FROM `Scene`{} " \
+          "GROUP BY `Satellite`, `Sensor` " \
+          "ORDER BY `Date`".format(where)
+
+    result = database('login', 'password', 'host', 'database').do_query(sql)
+
+    return result
 
 
 def get_browse_image(sceneid):
@@ -74,4 +128,8 @@ def get_browse_image(sceneid):
 
 
 class CollectionError(Exception):
+    pass
+
+
+class InvalidBoundingBoxError(Exception):
     pass
--- opensearch.py
+++ opensearch.py
@@ -3,12 +3,10 @@ import inpe_data
 import io
 
 app = Flask(__name__)
+app.config["APPLICATION_ROOT"] = "/opensearch"
 
-
-@app.route('/<string:format>/granule', methods=['GET'])
+@app.route('/granule.<string:format>', methods=['GET'])
 def os_granule(format):
-    if 'bbox' not in request.url:
-        abort(400, 'Parameter \'bbox\' is missing')
 
     data = []
     total_results = 0
@@ -16,22 +14,21 @@ def os_granule(format):
     count = int(request.args.get('count', 10))
 
     try:
-        bbox = request.args.get('bbox')
-        for coord in bbox.split(','):
-            float(coord)
-        data, total_results = inpe_data.get_bbox(bbox,
-                                                 request.args.get('q', None),
+
+        data, total_results = inpe_data.get_bbox(request.args.get('bbox', None),
+                                                 request.args.get('uid', None),
+                                                 request.args.get('collectionId'),
                                                  request.args.get('start', None),
                                                  request.args.get('end', None),
                                                  start_index, count)
-    except ValueError:
+    except inpe_data.InvalidBoundingBoxError:
         abort(400, 'Invalid bounding box')
     except IOError:
         abort(503)
     except inpe_data.CollectionError as e:
         abort(400, str(e))
 
-    resp = make_response(render_template('opensearch.' + format,
+    resp = make_response(render_template('granule.' + format,
                                          url=request.url.replace('&', '&amp;'),
                                          data=data, total_results=total_results,
                                          start_index=start_index, count=count,
@@ -43,41 +40,52 @@ def os_granule(format):
 
     return resp
 
-
-@app.route('/<string:format>/scene', methods=['GET'])
-def os_scene(format):
-    if 'sceneid' not in request.url:
-        abort(400, 'Parameter \'sceneid\' is missing')
+@app.route('/collections.<string:format>')
+def os_dataset(format):
     total_results = 0
-    data = []
+    data = None
+    start_index = int(request.args.get('startIndex', 0))
+    count = int(request.args.get('count', 10))
+
     try:
-        result = inpe_data.get_scene(request.args.get('sceneid'))
-        data.append(result)
+        result = inpe_data.get_datasets(request.args.get('keyword', None),
+                                        request.args.get('uid', None),
+                                        start_index, count)
+        total_results = len(result)
+        data = result
     except IOError:
         abort(503)
 
-    resp = make_response(render_template('opensearch.' + format,
+    resp = make_response(render_template('collections.' + format,
                                          url=request.url.replace('&', '&amp;'),
-                                         data=data, total_results=1,
-                                         start_index=0, count=1,
-                                         url_root=request.url_root))
-
+                                         data=data, total_results=total_results,
+                                         start_index=0, count=count,
+                                         url_root=request.url_root
+                                         ))
     if format == 'atom':
         format = 'atom+xml'
     resp.content_type = 'application/' + format
-
     return resp
 
 
-@app.route('/')
-@app.route('/osdd')
-def os_osdd():
-    resp = make_response(render_template('osdd_granule.xml', url=request.url_root))
+@app.route('/osdd/granule', defaults={'collection' : None})
+@app.route('/osdd/granule/<string:collection>')
+def os_osdd_granule(collection):
+
+    resp = make_response(render_template('osdd_granule.xml',
+                                         url=request.url_root,
+                                         collection=collection))
     resp.content_type = 'application/xml'
     return resp
 
+@app.route('/')
+@app.route('/osdd')
+@app.route('/osdd/collection')
+def os_osdd_collection():
+    resp = make_response(render_template('osdd_collection.xml', url=request.url_root))
+    resp.content_type = 'application/xml'
+    return resp
 
-@app.route('/opensearch/browseimage/<string:sceneid>')
 @app.route('/browseimage/<string:sceneid>')
 def browse_image(sceneid):
     try:
@@ -90,7 +98,6 @@ def browse_image(sceneid):
     return send_file(io.BytesIO(image), mimetype='image/jpeg')
 
 
-@app.route('/opensearch/metadata/<string:sceneid>')
 @app.route('/metadata/<string:sceneid>')
 def scene(sceneid):
     try:
@@ -130,4 +137,7 @@ def handle_page_not_found(e):
 
 
 if __name__ == '__main__':
+    app.jinja_env.trim_blocks = True
+    app.jinja_env.lstrip_blocks = True
+    app.jinja_env.keep_trailing_newline = True
     app.run(debug=True)
